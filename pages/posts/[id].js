dddca3c079e312d9bc6d16c01125675bcf5a8c39
jerry861200 <jerry861200@gmail.com>
2022-07-28 11:02:35 +0800

    feat: add date-fn

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,37 +1,41 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 import Layout from "../../components/layout";
-import { getAllPostIds, getPostData  } from "../../lib/posts";
+import Date from "../../components/date";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
 
-export default function Post({ postData}) {
-	const {title, id, date} = postData;
-  return <Layout>
-		<Head>
-			<title>{postData.title}</title>
-		</Head>
-		{title}
-		<br />
-		{id}
-		<br />
-		{date}
-		<br />
-		<div dangerouslySetInnerHTML={{ __html: postData.contentHtml}}></div>
-	</Layout>;
+export default function Post({ postData }) {
+  const { title, id, date } = postData;
+  return (
+    <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
+    </Layout>
+  );
 }
 
-export async function getStaticProps({ params }){
-	const postData = await getPostData(params.id);
-	return {
-		props:{
-			postData,
-		},
-	}
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
   const paths = getAllPostIds();
-	return {
-		paths,
-		fallback: false,
-	}
-};
+  return {
+    paths,
+    fallback: false,
+  };
+}
